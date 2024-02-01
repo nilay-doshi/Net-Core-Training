9a816daadae91df2e3326c5de88b5d8b648a55df
nilay-doshi <nilay.doshi@bacancy.com>
2024-02-01 16:23:20 +0530

    Add project files.

---
+++ HelloWorld1/HelloWorld1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace HelloWorld1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World";
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ demo1_webapp/demo1_webapp/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.Extensions.FileProviders;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+//static files code 
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+           Path.Combine(builder.Environment.ContentRootPath, "Img")),
+    RequestPath = "/Img"
+});
+//static files code over
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
